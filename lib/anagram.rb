c72d8ebbe8bfdc07cf09c343a4962b08d614c3ba
Kevin Dutzy <kddutz15@gmail.com>
2022-12-14 12:04:20 -0500

    commit tree 1d91c0d1fb265ffcb544121faea006d608d9bce6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+    attr_reader :book
+
+    def initialize(word)
+        @word = word
+
+    end
+
+    def match(array)
+        # return all matches in the array,
+        # if not matches - return empy array
+    end
+end
